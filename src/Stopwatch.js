635744c9fb8b0b656dcfbbcaec0d0c7477396664
mazuh <marcell-mz@hotmail.com>
2023-02-22 22:31:15 -0300

    implement stopwatch pause

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
@@ -6,37 +6,52 @@ import Button from "@mui/material/Button";
 export default function Stopwatch() {
   const [startedAt, setStartedAt] = useState(0);
   const start = () => setStartedAt(Date.now());
-  const reset = () => setStartedAt(null);
-  const pause = () => console.warn("Pause: not implemented.");
 
-  const [elapsedSeconds, setElapsedSeconds] = useState(0);
+  const [pausedAt, setPausedAt] = useState(0);
+
+  const getElapsedDelta = useCallback(
+    () => (startedAt ? Date.now() - startedAt : 0),
+    [startedAt]
+  );
+  const getPausedDelta = useCallback(
+    () => (pausedAt ? Date.now() - pausedAt : 0),
+    [pausedAt]
+  );
+
+  const pause = () => setPausedAt(Date.now());
+  const unpause = () =>
+    setPausedAt(0) & setStartedAt(startedAt + getPausedDelta());
+
+  const [visibleSeconds, setVisibleSeconds] = useState(0);
 
   useEffect(() => {
     const worker = setInterval(() => {
-      const elapsedMs = startedAt ? Date.now() - startedAt : 0;
-      const elapsingSeconds = Math.floor(elapsedMs / 1000);
-      setElapsedSeconds(elapsingSeconds);
-    }, 200);
+      const totalElapsedMiliseconds = getElapsedDelta() - getPausedDelta();
+      const totalElapsedSeconds = Math.floor(totalElapsedMiliseconds / 1000);
+      setVisibleSeconds(totalElapsedSeconds);
+    }, 100);
     return () => clearInterval(worker);
-  }, [startedAt]);
+  }, [getElapsedDelta, getPausedDelta]);
+
+  const clear = () => setVisibleSeconds(0) & setStartedAt(0) & setPausedAt(0);
 
   return (
     <Box component="section">
       <Typography variant="h4" component="p" p={2}>
-        {elapsedSeconds}
+        {visibleSeconds}
         <Typography variant="overline">s</Typography>
       </Typography>
-      {startedAt ? (
+      {startedAt && !pausedAt ? (
         <Button onClick={pause} variant="contained">
           Pause
         </Button>
       ) : (
-        <Button onClick={start} variant="contained">
+        <Button onClick={pausedAt ? unpause : start} variant="contained">
           Start
         </Button>
       )}{" "}
-      <Button onClick={reset} variant="outlined">
-        Reset
+      <Button onClick={clear} variant="outlined">
+        Clear
       </Button>
     </Box>
   );
