8a463570656596bb214fe646d9f1028ca84a9637
mazuh <marcell-mz@hotmail.com>
2023-02-22 22:44:36 -0300

    add decisecond precision to stopwatch
    fyi:
    1000 miliseconds may be called a second
    100 milliseconds may be called a decisecond
    10 milliseconds may be called a centisecond
    
    at least thats what wikipedia says

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -23,23 +23,32 @@ export default function Stopwatch() {
     setPausedAt(0) & setStartedAt(startedAt + getPausedDelta());
 
   const [visibleSeconds, setVisibleSeconds] = useState(0);
+  const [visibleDecisecond, setVisibleDecisecond] = useState(0);
 
   useEffect(() => {
     const worker = setInterval(() => {
-      const totalElapsedMiliseconds = getElapsedDelta() - getPausedDelta();
-      const totalElapsedSeconds = Math.floor(totalElapsedMiliseconds / 1000);
-      setVisibleSeconds(totalElapsedSeconds);
+      const miliseconds = getElapsedDelta() - getPausedDelta();
+      const roundedSeconds = Math.floor(miliseconds / 1000);
+      setVisibleSeconds(roundedSeconds);
+
+      const unvisibleMiliseconds = miliseconds - roundedSeconds * 1000;
+      const roundedDecisecond = Math.floor(unvisibleMiliseconds / 100);
+      setVisibleDecisecond(roundedDecisecond);
     }, 100);
     return () => clearInterval(worker);
   }, [getElapsedDelta, getPausedDelta]);
 
-  const clear = () => setVisibleSeconds(0) & setStartedAt(0) & setPausedAt(0);
+  const clear = () =>
+    setVisibleSeconds(0) &
+    setVisibleDecisecond(0) &
+    setStartedAt(0) &
+    setPausedAt(0);
 
   return (
     <Box component="section">
       <Typography variant="h4" component="p" p={2}>
         {visibleSeconds}
-        <Typography variant="overline">s</Typography>
+        <Typography variant="overline">s {visibleDecisecond}</Typography>
       </Typography>
       {startedAt && !pausedAt ? (
         <Button onClick={pause} variant="contained">
